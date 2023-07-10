afc073bce06408e1b01984b59d97f8f96b60cedb
kike_zempo <jose.zempoaltecatl1136@uppuebla.edu.mx>
2023-07-10 14:47:02 -0600

    imagenes svg

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+        dangerouslyAllowSVG: true,
+        contentDispositionType: 'attachment',
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    },
     env: {
         appName: "Tec-IA",
         appInsta: "https://www.instagram.com/",

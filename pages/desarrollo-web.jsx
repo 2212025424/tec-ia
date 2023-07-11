fe6d2fade6c88b3dd92605bb2e38d3e6b94d683f
kike_zempo <jose.zempoaltecatl1136@uppuebla.edu.mx>
2023-07-11 14:02:26 -0600

    contenido

--- pages/desarrollo-web.jsx
+++ pages/desarrollo-web.jsx
@@ -3,6 +3,7 @@ import Image from "next/image"
 export default function Desarrollo() {
     return (
         <main>
+
             <section className="mdf-bg-secondary mdf-clr-primary">
                 <div className="app-wrapper-content mdf-py-xx">
                     <div className="row">
@@ -13,7 +14,7 @@ export default function Desarrollo() {
                                 <li><i className="fa-solid fa-caret-right mdf-mr-sm mdf-mb-sm"></i> Tiendas en linea</li>
                                 <li><i className="fa-solid fa-caret-right mdf-mr-sm mdf-mb-sm"></i> Gestor de contenido</li>
                                 <li><i className="fa-solid fa-caret-right mdf-mr-sm mdf-mb-sm"></i> Blogs corporativos</li>
-                                <li><i className="fa-solid fa-caret-right mdf-mr-sm mdf-mb-sm"></i> Páginas OnePage</li>
+                                <li><i className="fa-solid fa-caret-right mdf-mr-sm mdf-mb-sm"></i> Páginas estáticas</li>
                             </ul>
                         </div>
                         <div className="col-md-5 mdf-fnt-center mdf-pt-xl">
@@ -25,38 +26,93 @@ export default function Desarrollo() {
 
             <section className="mdf-bg-primary">
                 <div className="app-wrapper-content mdf-py-xx">
-                    <h2 className="lyt-index-title mdf-fnt-h1 mdf-mb-xl">Lo incluye tu proyecto</h2>
-                    <h3 className="mdf-fnt-center mdf-fnt-h3 mdf-fnt-lighter mdf-mb-xl">Todos los proyectos que desarrollamos cumplen con lo siguiente para así lograr un entregable de calidad</h3>
+                    <h2 className="lyt-index-title mdf-fnt-h1 mdf-mb-xl">Sitios web estáticos</h2>
+                    <h3 className="mdf-fnt-center mdf-fnt-h3 mdf-fnt-lighter mdf-mb-xl">Estos proyectos son ideales cuando tu negocio no requiere estar actualizando información, sean productos, servicios, precios, etc de forma constante.</h3>
+
+                    <div className="mdf-grd-lg">
+                        <div className="cmp-simple-slate mdf-fnt-center" data-aos="zoom-in">
+                            <h3 className="mdf-fnt-h2 mdf-mb-md">Página OnePage</h3>
+                            <p className="mdf-fnt-md mdf-clr-light mdf-mb-md">Ideal cuando una empresa o negocio requiere comenzar a tener presencia en internet</p>
+                            <h2 className="mdf-fnt-lg mdf-clr-secondary mdf-mb-md">$ 5,500.00</h2>
+                            <p className="mdf-fnt-md mdf-mb-sm">1 única sección</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Hosting por un año</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Dominio por un año</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Enlace a sus redes sociales</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Imagen de portada</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Mapa de ubicación con google</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Testimonios max. 10</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Novedades max. 10</p>
+                            <p className="mdf-fnt-md mdf-mb-md">Galería de imágenes max. 10</p>
+                            <a className="cmp-main-button mdf-w-100 mdf-appbg-secondary mdf-clr-secondary-on" href="/">Solicitar información</a>
+                        </div>
+                        <div className="cmp-simple-slate mdf-fnt-center" data-aos="zoom-in">
+                            <h3 className="mdf-fnt-h2 mdf-mb-md">Emprendedor</h3>
+                            <p className="mdf-fnt-md mdf-clr-light mdf-mb-md">Ideal para dar a conocer su empresa de manera profesional y ofertar productos o servicios</p>
+                            <h2 className="mdf-fnt-lg mdf-clr-secondary mdf-mb-md">$ 7,500.00</h2>
+                            <p className="mdf-fnt-md mdf-mb-sm"><i class="fa-regular fa-thumbs-up mdf-clr-secondary mdf-mr-xm"></i> Hasta 5 secciones</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Hosting y dominio por un año</p>
+                            <p className="mdf-fnt-md mdf-mb-sm"><i class="fa-regular fa-thumbs-up mdf-clr-secondary mdf-mr-xm"></i> Formulario de contacto</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Enlace a sus redes sociales</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Imagen de portada</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Mapa de ubicación con google</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Testimonios max. 10</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Novedades max. 10</p>
+                            <p className="mdf-fnt-md mdf-mb-md">Galería de imágenes max. 10</p>
+                            <a className="cmp-main-button mdf-w-100 mdf-appbg-secondary mdf-clr-secondary-on" href="/">Solicitar información</a>
+                        </div>
+                        <div className="cmp-simple-slate mdf-fnt-center" data-aos="zoom-in">
+                            <h3 className="mdf-fnt-h2 mdf-mb-md">Empresarial</h3>
+                            <p className="mdf-fnt-md mdf-clr-light mdf-mb-md">Ideal cuando una empresa o negocio requiere comenzar a tener presencia en internet</p>
+                            <h2 className="mdf-fnt-lg mdf-clr-secondary mdf-mb-md">$ 12,500.00</h2>
+                            <p className="mdf-fnt-md mdf-mb-sm"><i class="fa-regular fa-thumbs-up mdf-clr-secondary mdf-mr-xm"></i> Hasta 10 secciones</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Hosting y dominio por un año</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Formulario de contacto</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Enlace a sus redes sociales</p>
+                            <p className="mdf-fnt-md mdf-mb-sm"><i class="fa-regular fa-thumbs-up mdf-clr-secondary mdf-mr-xm"></i> Imagen de portada dinámica</p>
+                            <p className="mdf-fnt-md mdf-mb-sm">Mapa de ubicación con google</p>
+                            <p className="mdf-fnt-md mdf-mb-sm"><i class="fa-regular fa-thumbs-up mdf-clr-secondary mdf-mr-xm"></i> Gestor de testimonios</p>
+                            <p className="mdf-fnt-md mdf-mb-sm"><i class="fa-regular fa-thumbs-up mdf-clr-secondary mdf-mr-xm"></i> Gestor de novedades</p>
+                            <p className="mdf-fnt-md mdf-mb-md">Galería de imágenes max. 10</p>
+                            <a className="cmp-main-button mdf-w-100 mdf-appbg-secondary mdf-clr-secondary-on" href="/">Solicitar información</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="mdf-bg-secondary">
+                <div className="app-wrapper-content mdf-py-xx">
+                    <h2 className="lyt-index-title mdf-fnt-h1 mdf-mb-xl">Todo proyecto incluye:</h2>
+                    <h3 className="mdf-fnt-center mdf-fnt-h3 mdf-fnt-lighter mdf-mb-xl">Sitios web estático o dimámicos que desarrollamos cumplen con lo siguiente para así lograr un entregable de calidad</h3>
 
                     <div className="mdf-grd-lg">
                         <div className="cmp-simple-slate" data-aos="zoom-in">
                             <h3 className="mdf-fnt-md mdf-mb-md">Diseño responsivo</h3>
-                            <p className="mdf-fnt-sm mdf-mb-lg">Elaboramos diseños responsivos y amigables que cumplen criterios de calidad actuales en usabilidad y accesibilidad.</p>
+                            <p className="mdf-fnt-sm mdf-mb-lg">En todo diseño se contempla que las interfaces puedan adaptarse a distintas medidas de dispositivos con los que un usuario podría acceder a su contenido.</p>
                             <figure className="mdf-fnt-center"><Image src="/images/undraw_real_time_sync_re_nky7.svg" alt="Imagen de diseño web" className="mdf-img-responsive" width={130} height={130} /></figure>
                         </div>
                         <div className="cmp-simple-slate" data-aos="zoom-in">
                             <h3 className="mdf-fnt-md mdf-mb-md">Desarrollo personalizado</h3>
-                            <p className="mdf-fnt-sm mdf-mb-lg">Desarrollamos proyectos de desarrollo web profesionales, páginas web estáticas, tiendas en linea, blogs, CMS.</p>
+                            <p className="mdf-fnt-sm mdf-mb-lg">Con base en sus necesidades y criterios se determinan los diseños apropiados para su negocio o empresa, contemplando criterios de usabilidad y accesibilidad.</p>
                             <figure className="mdf-fnt-center"><Image src="/images/undraw_building_blocks_re_5ahy.svg" alt="Imagen de desarrollo web" className="mdf-img-responsive" width={115} height={115} /></figure>
                         </div>
                         <div className="cmp-simple-slate" data-aos="zoom-in">
                             <h3 className="mdf-fnt-md mdf-mb-md">Optimización de SEO</h3>
-                            <p className="mdf-fnt-sm mdf-mb-lg">Hacemos copias de seguridad, agregamos nuevas funcionalidades y cambiamos el diseños de sitios ya construidos.</p>
+                            <p className="mdf-fnt-sm mdf-mb-lg">Se contemplan los criterios de calidad actuales propuestos por la comunidad de Google, porque estos tienen como finalidad servir contenido de forma óptima.</p>
                             <figure className="mdf-fnt-center"><Image src="/images/undraw_search_re_x5gq.svg" alt="Imagen de mantenimiento de cómputo" className="mdf-img-responsive" width={135} height={135} /></figure>
                         </div>
                         <div className="cmp-simple-slate" data-aos="zoom-in">
                             <h3 className="mdf-fnt-md mdf-mb-md">Código optimizado</h3>
-                            <p className="mdf-fnt-sm mdf-mb-lg">Agregamos capañas publicitarias a tu sitio en espacios estratégicos para que logres generar dinero con tu contenido.</p>
+                            <p className="mdf-fnt-sm mdf-mb-lg">Cada una de sus solicitudes se desarrollan con las mejores prácticas de programación con la finalidad de hacer que su aplicación funcione de manera eficiente.</p>
                             <figure className="mdf-fnt-center"><Image src="/images/undraw_proud_coder_re_exuy.svg" alt="Imagen de monetización web" className="mdf-img-responsive" width={170} height={170} /></figure>
                         </div>
                         <div className="cmp-simple-slate" data-aos="zoom-in">
                             <h3 className="mdf-fnt-md mdf-mb-md">Soporte y capacitación</h3>
-                            <p className="mdf-fnt-sm mdf-mb-lg">Implementamos Google Analytics para analizar el tráfico de tu sitio web, así logras concer más a tus visitantes.</p>
+                            <p className="mdf-fnt-sm mdf-mb-lg">Conluído el proyecto, ofrecemos soporte y capacitación para el uso correcto, conocerá los términos más relevantes de la web y la tecnología de su proyecto.</p>
                             <figure className="mdf-fnt-center"><Image src="/images/undraw_teaching_re_g7e3.svg" alt="Imagen de análisis de tráfico" className="mdf-img-responsive" width={150} height={150} /></figure>
                         </div>
                         <div className="cmp-simple-slate" data-aos="zoom-in">
                             <h3 className="mdf-fnt-md mdf-mb-md">Soporte SSL</h3>
-                            <p className="mdf-fnt-sm mdf-mb-lg">Realizamos análisis de sitios, nos aseguramos que cumplan los criterios de calidad propuestos por navegadores.</p>
+                            <p className="mdf-fnt-sm mdf-mb-lg">En la puesta en producción se brinda un certificado de seguridad (SSL), para que así nuestra información se mantenga integra a cualquier amenaza en la red.</p>
                             <figure className="mdf-fnt-center"><Image src="/images/undraw_secure_login_pdn4.svg" alt="Imagen de sitios de calidad" className="mdf-img-responsive" width={150} height={150} /></figure>
                         </div>
                     </div>
